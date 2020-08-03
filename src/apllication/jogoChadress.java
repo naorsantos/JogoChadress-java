374e87615880d003d6175c26a95e5845100dd271
Naor Santos <naorsantos@gmail.com>
2020-08-03 19:45:09 -0300

    Primeira classe position

--- src/apllication/jogoChadress.java
+++ src/apllication/jogoChadress.java
@@ -1,10 +1,13 @@
 package apllication;
 
+import BoardGame.Position;
+
 public class jogoChadress {
 
 	public static void main(String[] args) {
 	
-
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 	}
 
 }
